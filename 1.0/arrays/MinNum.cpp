4bb42e63fdb2c2f60cdb892b6c1a197877d1933d
MOHDSAIFGIT <mohdsaifb173@gmail.com>
2023-07-17 11:23:26 +0530

    MinNum

---
+++ 1.0/arrays/MinNum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+
+int main(){
+
+    int n = 5;
+    int Min = INT_MAX;
+    int arr[n] = {-4,-33,-6,-3,-22};
+
+    for(int i=0;i<n;i++){
+
+        if(arr[i]<Min){
+            Min = arr[i];
+            
+        }
+    
+    }
+    cout<<"The Greatest element of the array is :"<<Min;
+    return 0;
+}
