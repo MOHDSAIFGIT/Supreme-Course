2466768a6254470bf2d903199cd0317328049b9d
MOHDSAIFGIT <mohdsaifb173@gmail.com>
2023-07-18 13:50:41 +0530

    Union

---
+++ 1.0/arrays/Union.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std ;
+
+int main(){
+    int arr[] = {1,2,3,4,5};
+    int asize = 5;
+    int brr[] = {6,7,8,9};
+    int bsize = 4;
+
+    vector<int> ans;
+
+    for(int i =0;i<asize;i++){
+
+        ans.push_back(arr[i]);
+    }
+
+    for(int i=0;i<bsize;i++){
+
+        ans.push_back(brr[i]);
+    }
+
+    cout<<"printing Union : ";
+    for(int i=0;i<ans.size();i++){
+        cout<< ans[i] << " ";
+    }
+}
