72a83de02079f1f0a9e16b470fcfce6e1fecef6b
MOHDSAIFGIT <mohdsaifb173@gmail.com>
2023-07-21 21:46:35 +0530

    alignment

--- 1.0/arrays/Union.cpp
+++ 1.0/arrays/Union.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <vector>
-using namespace std ;
+using namespace std;
 
-int main(){
-    int arr[] = {1,2,3,4,5};
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
     int asize = 5;
-    int brr[] = {6,7,8,9};
+    int brr[] = {6, 7, 8, 9};
     int bsize = 4;
 
     vector<int> ans;
 
-    for(int i =0;i<asize;i++){
+    for (int i = 0; i < asize; i++)
+    {
 
         ans.push_back(arr[i]);
     }
 
-    for(int i=0;i<bsize;i++){
+    for (int i = 0; i < bsize; i++)
+    {
 
         ans.push_back(brr[i]);
     }
 
-    cout<<"printing Union : ";
-    for(int i=0;i<ans.size();i++){
-        cout<< ans[i] << " ";
+    cout << "printing Union : ";
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
     }
 }
