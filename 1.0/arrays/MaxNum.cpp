06b7f2604ddae5103ea5c40f9c712b3ffda07656
MOHDSAIFGIT <mohdsaifb173@gmail.com>
2023-07-22 20:17:54 +0530

    Alighnment

--- 1.0/arrays/MaxNum.cpp
+++ 1.0/arrays/MaxNum.cpp
@@ -1,21 +1,22 @@
-#include<iostream>
-#include<limits.h>
+#include <iostream>
+#include <limits.h>
 using namespace std;
 
-int main(){
+int main()
+{
 
     int n = 5;
     int Max = INT_MIN;
-    int arr[n] = {-4,-3,-6,-3,-22};
+    int arr[n] = {-4, -3, -6, -3, -22};
 
-    for(int i=0;i<n;i++){
+    for (int i = 0; i < n; i++)
+    {
 
-        if(arr[i]>Max){
+        if (arr[i] > Max)
+        {
             Max = arr[i];
-            
         }
-    
     }
-    cout<<"The Greatest element of the array is :"<<Max;
+    cout << "The Greatest element of the array is :" << Max;
     return 0;
 }
