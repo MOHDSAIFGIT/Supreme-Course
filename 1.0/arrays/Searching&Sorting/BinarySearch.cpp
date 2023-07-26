cf8c68b47d28c7f9d3da7ad11d3e4f50fc1a576e
MOHDSAIFGIT <mohdsaifb173@gmail.com>
2023-07-26 23:56:01 +0530

    BinarySearch

---
+++ 1.0/arrays/Searching&Sorting/BinarySearch.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+
+int binarySearch(int arr[],int size,int key){
+    int start = 0;
+    int end = size-1;
+
+    int mid = (start+end)/2;
+
+    while(start<=end){
+        int ele = arr[mid];
+
+        if(ele ==key){
+            return mid;
+        }
+        else if(key<ele){
+            end = mid-1;
+        }
+        else{
+            start = mid +1;
+        }
+
+        mid = (start+end)/2;
+    }
+
+    //element not found 
+    return -1;
+
+}
+
+int main(){
+
+    int arr[]={2,4,6,8,10,12,16};
+    int size=7;
+    int key=2;
+
+    int indexOftarget = binarySearch(arr,size,key);
+
+    if(indexOftarget == -1){
+        cout<<"key not found";
+    }
+    else{
+        cout<<"key found at "<<indexOftarget<<" index"<< endl;
+    }
+    return 0;
+}
