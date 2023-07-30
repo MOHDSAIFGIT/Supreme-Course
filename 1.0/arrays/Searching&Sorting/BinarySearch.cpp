cc28195385a3e0e8aaf3aabb3618d8d2841be523
MOHDSAIFGIT <mohdsaifb173@gmail.com>
2023-07-30 12:48:21 +0530

    BinarySearch

--- 1.0/arrays/Searching&Sorting/BinarySearch.cpp
+++ 1.0/arrays/Searching&Sorting/BinarySearch.cpp
@@ -6,7 +6,8 @@ int binarySearch(int arr[],int size,int key){
     int start = 0;
     int end = size-1;
 
-    int mid = (start+end)/2;
+    // int mid = (start+end)/2;    this is wromg method to find mid (interger overflow issue occur)
+    int mid = start + (end-start)/2;
 
     while(start<=end){
         int ele = arr[mid];
