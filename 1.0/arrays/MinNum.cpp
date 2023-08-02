b5cd288159c39f6f58ba9c6c314019114089eccd
MOHDSAIFGIT <mohdsaifb173@gmail.com>
2023-08-02 22:06:55 +0530

    Delete a file

--- 1.0/arrays/MinNum.cpp
+++ 1.0/arrays/MinNum.cpp
@@ -1,21 +1,22 @@
-#include<iostream>
-#include<limits.h>
+#include <iostream>
+#include <limits.h>
 using namespace std;
 
-int main(){
+int main()
+{
 
     int n = 5;
     int Min = INT_MAX;
-    int arr[n] = {-4,-33,-6,-3,-22};
+    int arr[n] = {-4, -33, -6, -3, -22};
 
-    for(int i=0;i<n;i++){
+    for (int i = 0; i < n; i++)
+    {
 
-        if(arr[i]<Min){
+        if (arr[i] < Min)
+        {
             Min = arr[i];
-            
         }
-    
     }
-    cout<<"The Greatest element of the array is :"<<Min;
+    cout << "The Greatest element of the array is :" << Min;
     return 0;
 }
