7ddf7058a517af18b54ca0cd3b73802af0019952
MOHDSAIFGIT <mohdsaifb173@gmail.com>
2023-08-06 23:05:26 +0530

    Reverse String

---
+++ 1.0/arrays/String/ReverseString.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int getLength(char name[])
+{
+    int length = 0;
+    int i = 0;
+
+    while (name[i] != '\0')
+    {
+        length++;
+        i++;
+    }
+    return length;
+}
+int reverseChar(char name[])
+{
+    int i = 0;
+    int n = getLength(name);
+    int j = n - 1;
+
+    while (i <= j)
+    {
+        swap(name[i], name[j]);
+        i++;
+        j--;
+    }
+    // return name;
+}
+
+int main()
+{
+    char name[100];
+    cin >> name;
+
+    cout << "length is" << getLength(name) << endl;
+
+    cout << "initially: " << name << endl;
+    reverseChar(name);
+    cout << "After Reverse" << name << endl;
+
+    return 0;
+}
