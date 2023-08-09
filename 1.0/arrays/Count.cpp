b428c5c34faa22d9b288cb881d7da27d0d99e234
MOHDSAIFGIT <mohdsaifb173@gmail.com>
2023-08-09 22:00:23 +0530

    adding code

--- 1.0/arrays/Count.cpp
+++ 1.0/arrays/Count.cpp
@@ -1,25 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
 
-    int numZero = 0,n=13,numOne = 0;
-    int arr[n]={0,1,1,1,0,0,0,0,1,0,1,0,1};
-    
-    for(int i = 0;i<n;i++){
-        if(arr[i] == 1){
+    int numZero = 0, n = 13, numOne = 0;
+    int arr[n] = {0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1};
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 1)
+        {
             numOne++;
         }
     }
-    for(int i=0;i<n;i++){
-        if(arr[i]==0){
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 0)
+        {
             numZero++;
         }
     }
 
-    cout<<"The total zeros are : "<<numZero<<endl;
-    cout<<"The total Ones are : "<<numOne<<endl;
+    cout << "The total zeros are : " << numZero << endl;
+    cout << "The total Ones are : " << numOne << endl;
     return 0;
-
 }
