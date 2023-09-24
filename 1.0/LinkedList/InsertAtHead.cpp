16c4c181cc102bada4c58e1fa8b63ec054939311
MOHDSAIFGIT <mohdsaifb173@gmail.com>
2023-09-24 12:26:09 +0530

    InsertAtHead + InsertAtTail

--- 1.0/LinkedList/InsertAtHead.cpp
+++ 1.0/LinkedList/InsertAtHead.cpp
@@ -31,23 +31,50 @@ void print(Node *head)
 }
 
 
-void insertAtHead(Node * &head , int data){
-
+void insertAtHead(Node * &head ,Node* &tail, int data){
+     
     // step 1 : Create a Node 
     Node* newNode = new Node(data);
     //Step 2 : NewNode-> next = head
     newNode -> next = head;
     //Step 3 : head = newNode
+    //first node
+    if(head == NULL){
+        tail = newNode;
+    }
     head = newNode;
+}
+
+void insertAtTail(Node * &head,Node* &tail , int data){
+    //step 1: Create the Node
+    Node* newNode = new Node(data);
+
+    //step 2: Connect with tail node
+    if(tail==NULL){
+        tail = newNode;
+    }
+    else{
+        tail->next = newNode;
+        head = newNode;
+    }
+    
+    //step 3: update tail
+
+    if(head == NULL){
+        head = newNode; 
+    }
+    tail = newNode;
 }
 
 int main(){
 
-    Node* head = new Node(10);
-    insertAtHead(head,20);
-    insertAtHead(head,50);
-    insertAtHead(head,60);
-    insertAtHead(head,90);
+    Node* head = NULL;
+    Node* tail = NULL;
+    // insertAtHead(head,tail,20);
+    // insertAtHead(head,tail,50);
+    // insertAtHead(head,tail,60);
+    // insertAtHead(head,tail,90);
+    insertAtTail(head,tail,77);
 
     print(head);
     return 0;
