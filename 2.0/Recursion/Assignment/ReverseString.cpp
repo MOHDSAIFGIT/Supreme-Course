b9f9e8999abf34aed3b9a281fa88800bd56a15fc
MOHDSAIFGIT <mohdsaifb173@gmail.com>
2023-10-19 19:01:48 +0530

    Reverse String using RE

---
+++ 2.0/Recursion/Assignment/ReverseString.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std ;
+
+void reverse(string&s ,int start,int end){
+
+    //base case
+    if (start >=end) return;
+
+    //ek case solve
+    swap(s[start],s[end]);
+
+
+    // RE
+    reverse(s,start+1,end-1);
+}
+
+int main(){
+
+    string s ;
+    cin >> s;
+
+    reverse(s,0,s.size()-1);
+
+    cout<<s;
+    return 0;
+}
